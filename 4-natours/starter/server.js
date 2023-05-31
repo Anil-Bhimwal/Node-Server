4c1844802201b797ab93e3999d448ab3b30d5af1
Anil Bhimwal <anilbhimwal@MacBook-Pro-4.local>
2023-05-31 17:47:39 +0530

    env variable

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,8 @@
+require('dotenv').config({ path: './config.env' });
 const app = require('./app');
 /** Start Server */
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
